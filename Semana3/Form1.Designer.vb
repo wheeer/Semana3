7878c9c3032582812aa60bf3693a1f9f984b1b5c
Wheeer <whollub16@hotmail.com>
2025-09-05 03:59:22 -0400

    Agregar archivos de proyecto.

---
+++ Semana3/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btCalcular = New Button()
+        cbOperaciones = New ComboBox()
+        tbNum2 = New TextBox()
+        tbNum1 = New TextBox()
+        Label1 = New Label()
+        lbResul = New Label()
+        SuspendLayout()
+        ' 
+        ' btCalcular
+        ' 
+        btCalcular.Location = New Point(463, 188)
+        btCalcular.Name = "btCalcular"
+        btCalcular.Size = New Size(75, 23)
+        btCalcular.TabIndex = 0
+        btCalcular.Text = "Calcular"
+        btCalcular.UseVisualStyleBackColor = True
+        ' 
+        ' cbOperaciones
+        ' 
+        cbOperaciones.FormattingEnabled = True
+        cbOperaciones.Location = New Point(336, 189)
+        cbOperaciones.Name = "cbOperaciones"
+        cbOperaciones.Size = New Size(121, 23)
+        cbOperaciones.TabIndex = 1
+        ' 
+        ' tbNum2
+        ' 
+        tbNum2.Location = New Point(230, 188)
+        tbNum2.Name = "tbNum2"
+        tbNum2.Size = New Size(100, 23)
+        tbNum2.TabIndex = 2
+        ' 
+        ' tbNum1
+        ' 
+        tbNum1.Location = New Point(230, 159)
+        tbNum1.Name = "tbNum1"
+        tbNum1.Size = New Size(100, 23)
+        tbNum1.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(151, 167)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(73, 15)
+        Label1.TabIndex = 4
+        Label1.Text = " 1er Numero"
+        ' 
+        ' lbResul
+        ' 
+        lbResul.AutoSize = True
+        lbResul.Location = New Point(233, 225)
+        lbResul.Name = "lbResul"
+        lbResul.Size = New Size(0, 15)
+        lbResul.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(lbResul)
+        Controls.Add(Label1)
+        Controls.Add(tbNum1)
+        Controls.Add(tbNum2)
+        Controls.Add(cbOperaciones)
+        Controls.Add(btCalcular)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btCalcular As Button
+    Friend WithEvents cbOperaciones As ComboBox
+    Friend WithEvents tbNum2 As TextBox
+    Friend WithEvents tbNum1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lbResul As Label
+
+End Class
---
+++ Semana3/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cbOperaciones.Items.Add("Sumar")
+        cbOperaciones.Items.Add("Restar")
+        cbOperaciones.Items.Add("Multiplicar")
+        cbOperaciones.Items.Add("Dividir")
+        cbOperaciones.SelectedIndex = 0
+    End Sub
+    Private Sub btCalcular_Click(sender As Object, e As EventArgs) Handles btCalcular.Click
+        EjecutarOperacion()
+    End Sub
+
+    Private Sub EjecutarOperacion()
+        Dim num1, num2, resultado As Double
+        num1 = Convert.ToDouble(tbNum1.Text)
+        num2 = Convert.ToDouble(tbNum2.Text)
+        Select Case cbOperaciones.SelectedItem.ToString()
+            Case "Sumar"
+                resultado = num1 + num2
+            Case "Restar"
+                resultado = num1 - num2
+            Case "Multiplicar"
+                resultado = num1 * num2
+            Case "Dividir"
+                If num2 <> 0 Then
+                    resultado = num1 / num2
+                Else
+                    MessageBox.Show("No se puede dividir por cero.")
+                    Return
+                End If
+            Case Else
+                MessageBox.Show("Operación no válida.")
+                Return
+        End Select
+        lbResul.Text = "Resultado: " & resultado.ToString()
+    End Sub
+End Class
